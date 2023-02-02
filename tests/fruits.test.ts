1147da73ebfeb0736cf49c017180bf54e3bb1d29
BrVecchi <bno.vecchi@gmail.com>
2023-02-02 11:25:39 -0300

    feat: more tests

--- tests/fruits.test.ts
+++ tests/fruits.test.ts
@@ -1,3 +1,4 @@
+import { response } from 'express';
 import { app } from 'index';
 import { FruitInput } from 'services/fruits-service';
 import supertest from 'supertest';
@@ -12,7 +13,23 @@ describe('Fruits tests', () => {
 
     it('Should create a valid fruit', async () => {
         const result = await api.post('/fruits').send(body)
-
         expect(result.status).toBe(201)
+    })
+
+    it('Should not create the same fruit', async () => {
+        const result = await api.post('/fruits').send(body)
+        expect(result.status).toBe(409)
+    })
+
+    it('Should return all fruits', async () => {
+        const result = await api.get('/fruits')
+        const response =  result.body
+        expect(result.status).toBe(200)
+        expect(response.length).toBe(1)
+    })
+
+    it('Should return a specific fruit', async () => {
+        const result = await api.get('/fruits/1')
+        expect(result.status).toBe(200)
     })
 })
